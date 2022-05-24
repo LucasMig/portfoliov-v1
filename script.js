10764b0bda1f84da4748386b0ef60e90c1d4e819
LucasMig <lucasmigliori@gmail.com>
2022-05-24 10:07:14 -0300

    mobile version finished

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
 "use strict";
 
-//SMOOTH SCROLLING
+//DECLARING VARIABLES
+const mainApp = document.querySelector(".main__app");
 const nav = document.querySelector(".nav--primary");
 const footerLink = document.querySelector(".footer--link");
+const displaySelector = document.getElementById("checkbox");
+const btnOpenMenu = document.getElementById("btn__menu--open");
+const btnCloseMenu = document.getElementById("btn__menu--close");
+const mobileNav = document.querySelector(".nav__container");
+const date = document.getElementById("date");
+const curYear = new Date().getFullYear();
 
+//SMOOTH SCROLLING
 const scrollToTarget = function (event) {
   event.preventDefault();
   const target = document.querySelector(event.target.getAttribute("href"));
 
   target.scrollIntoView({ behavior: "smooth" });
+  closeMobileMenu();
+};
+
+const openMobileMenu = function () {
+  mobileNav.classList.add("active");
+};
+
+const closeMobileMenu = function () {
+  mobileNav.classList.remove("active");
 };
 
 nav.addEventListener("click", scrollToTarget);
 footerLink.addEventListener("click", scrollToTarget);
 
 // DISPLAY MODE SWITCH
-const displaySelector = document.getElementById("checkbox");
-
 displaySelector.addEventListener("change", () => {
   document.body.classList.toggle("dark");
 });
 
-//KEEP FOOTER DATE UPDATED
-const date = document.getElementById("date");
-const curYear = new Date().getFullYear();
+//MOBILE NAVIGATION
+btnOpenMenu.addEventListener("click", function (event) {
+  event.preventDefault();
+  openMobileMenu();
+});
 
+btnCloseMenu.addEventListener("click", function (event) {
+  event.preventDefault();
+  closeMobileMenu();
+});
+
+//KEEP FOOTER DATE UPDATED
 date.innerHTML = curYear;
